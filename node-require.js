f599557d86d0519f85a6e7e7e77bf77cc963d85d
Michael Herman <michael@mherman.org>
2013-10-14 11:30:39 -0600

    require

--- node-require.js
+++ node-require.js
@@ -1,11 +1,15 @@
 // Load the http module to create an http server.
 var http = require('http');
+var fs = require('fs');
  
 // Configure our HTTP server to respond with Hello World to all requests.
 var server = http.createServer(function (request, response) {
   // response.writeHead(200, {"Content-Type": "text/plain"});
   response.writeHead(200, {"Content-Type": "text/html"});
-  response.end("<h1>Hello, Matt Null.</h1>\n");
+  // response.end("<h1>Hello, Matt Null.</h1>\n");
+  var data = fs.readFileSync('data.txt','utf8');
+  console.log(data)
+  response.end(data)
 });
  
 // Listen on port 8000, IP defaults to 127.0.0.1
